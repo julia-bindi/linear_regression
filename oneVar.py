a82ffd9581622c6a6621b19a3cb67b218ff5c9b0
julia-bindi <juliabindi@gmail.com>
2023-03-31 21:59:17 -0300

    fix linear regression

--- oneVar.py
+++ oneVar.py
@@ -3,11 +3,12 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-def plot_2d(results: np.array, expected = [], labels=[''], title='', block=False):
+def plot_2d(results: np.array, expected:np.array, labels=[''], title='', block=False):
 
     plt.figure()
 
-    plt.plot(results.T[0], results.T[1], 'xr', label=labels[0])
+    if len(results):
+        plt.plot(results.T[0], results.T[1], 'xr', label=labels[0])
     if len(expected):
         plt.plot(expected.T[0], expected.T[1], '-b', label=labels[1])
 
@@ -23,31 +24,42 @@ class Regression:
         self.alpha = alpha
         self.epocas = epocas
         self.df = pd.read_csv(filepath_or_buffer=file, names=labels)
+        self.labels = labels
         self.featuresNumber = featuresNumber
+        self.E = [0 for i in range(epocas)]
 
     def h(self, x = []):
-        return self.t[0] + sum(self.t[i]*x[i] for i in range(self.featuresNumber))
+        return self.t[0] + sum(self.t[i+1]*x[i] for i in range(self.featuresNumber))
+
+    def plotData(self):
+        plot_2d(results=np.array(self.df), expected=[], labels=self.labels, block=True)
+
+    def plotRegression2d(self):
+        plot_2d(results=np.array(self.df), expected=np.array([[self.df.T[:-1][i],self.h(self.df.T[:-1][i])] for i in range(len(self.df))]), labels=self.labels, block=True)
+
+    def plotError(self):
+        plot_2d(results=[], expected=np.array([[i,self.E[i]] for i in range(self.epocas)]), labels=['Época', 'Erro'], block=True)
 
     def execute(self):
-
-        E = [0 for i in range(self.epocas)]
 
         for epoca in range(0, self.epocas):
 
             Ex = [0 for j in range(self.featuresNumber+1)]
 
-            for i in range(len(self.df.T[0])):
+            for i in range(len(self.df)):
                 error = self.h(self.df.T[:-1][i]) - self.df.T[i][-1]
-                E[epoca] += np.power(error, 2) / (2 * len(self.df))
-                Ex[0] += error
+                self.E[epoca] += np.power(error, 2) / (2 * len(self.df))
+                Ex[0] += error / (len(self.df))
                 for j in range(self.featuresNumber):
                     Ex[j+1] += (error * self.df.T[:-1][i][j]) / (len(self.df))
 
             for i in range(self.featuresNumber+1):
-                self.t[i] = self.t[i] + self.alpha * Ex[i]
+                self.t[i] = self.t[i] - self.alpha * Ex[i]
 
         print(self.t)
-        plot_2d(results=np.array(self.df), expected=[], labels=['população', 'lucro'], block=True)
 
-rg = Regression(0.1, 100, "./data1.txt", ['population', 'profit'])
+rg = Regression(0.01, 1000, "./data1.txt", ['population', 'profit'])
 rg.execute()
+rg.plotData()
+rg.plotRegression2d()
+rg.plotError()
